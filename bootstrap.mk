fc7eb469ca87e5d1b402622fb90c401945c4e1b1
Mac Radigan <na>
2017-06-12 02:53:34 -0700

    added bootstrap file

---
+++ bootstrap.mk
@@ -0,0 +1,22 @@
+#!/usr/bin/make -f
+## boostrap.mk (for tecla)
+## Mac Radigan
+
+.PHONY: bootstrap build update packages-apt install
+.DEFAULT_GOAL := bootstrap
+
+bootstrap:
+	#autoreconf --force --install
+
+bulid: bootstrap
+	./configure && make
+
+install: build
+	sudo make install
+
+update:
+	$(MAKE) -C ./submodules update
+
+packages-apt:
+
+## *EOF*
